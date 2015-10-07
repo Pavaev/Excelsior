bca1279606397f9fe0e525d8ec3f622669b3c055
Panaev <28530@mail.ru>
2015-10-08 00:31:02 +0300

    First commit

---
+++ myServlet/src/main/java/ru/excelsior/servlet/ServletForm.java
@@ -0,0 +1,61 @@
+package ru.excelsior.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+
+public class ServletForm extends HttpServlet {
+    public static final File DAT = new File("D:\\dat.txt");
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE html>" + "<html>" + "<head>"
+                + "<title>Registration</title>" + "<meta charset=\"utf-8\">"
+                + "<body>" + "<form method=\"POST\">" + "<p>" + "<input type=\"email\" name=\"email\"><br>"
+                + "<input type=\"password\" name=\"password\"></p>"
+                + "<p><input type=\"submit\" ></p>" + "<p><input type=\"radio\" value=\"Women\" name=\"sex\"> Man</p>"
+                + "<p><input type=\"radio\" value=\"Women\" name=\"sex\"Women</p></body></html>Woman</p>"
+                + "<p><input type=\"checkbox\" name=\"checkbox\" style=\"height:10px; width:10px;\">Подписка</input></p>"
+                + "</body></html>");
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+
+        saveData(req.getParameter("email"), req.getParameter("password"), req.getParameter("sex"), req.getParameter("checkbox"));
+    }
+
+    protected void saveData(String email, String password, String radio, String checkbox) {
+
+        try (FileWriter writer = new FileWriter(DAT, true)) {
+            writer.write(email + " ");
+            writer.write(password + " ");
+            if (checkbox == null) {
+            writer.write("not_chosen ");}
+            if (checkbox == null) {
+                writer.write("off ");
+            } else {
+                writer.write(checkbox);
+            }
+            writer.append("\n");
+
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+
+}
---
+++ myServlet/src/main/java/ru/excelsior/servlet/ServletShow.java
@@ -0,0 +1,52 @@
+package ru.excelsior.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ServletShow extends HttpServlet {
+
+
+    protected ArrayList<String> datSection()throws FileNotFoundException {
+        ArrayList<String> list = new ArrayList<String>();
+        Scanner scan = new Scanner(ServletForm.DAT);
+        while(scan.hasNext()==true) {
+            list.add(scan.next());
+        }
+        return list;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE html>" + "<html>" + "<head>"
+                + "<title>Table</title>" + "<meta charset=\"utf-8\">"
+                + "<body><table cellpadding=\"7\" border=\"2\">"
+                + "<tr><th>email</th><th>password</th><th>sex</th><th>subscription</th></tr>");
+
+
+        ArrayList<String> list = datSection();
+                int q =0;
+        for (int i = 0; i < list.size(); i++) {
+            out.println("<tr>");
+            for (int j = 0; j < 4; j++) {
+                out.println("<td>" + list.get(q++) + "</td>");
+            }
+            out.println("</tr>");
+        }
+        out.println("</table></body></html>");
+        out.close();
+
+    }
+
+
+}
+
+
